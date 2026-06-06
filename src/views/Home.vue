11b97924c995d1e00a1902b74cf89ef8a63b8e6d
axitha-tec <mudavathuaxitha@gmail.com>
2026-06-06 22:39:48 +0530

    Customize Reclaim homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,71 +1,71 @@
 <template lang="pug">
 div
   b-alert(v-if="$isAndroid" show)
-    | #[b Note:] ActivityWatch on Android is in a very early stage of development. There will be bugs, but we hope you bear with us as we refine things and get it on par with the desktop version of ActivityWatch (which you should try!).
+    | #[b Note:] Reclaim Mobile is currently under development.
 
-  h3 Hello early user,
-  p
-    | It's still early days for ActivityWatch#[span(v-if="$isAndroid") (especially on Android!)]. We've come a long way but we need users (like you!)
-    | to provide feedback and help us turn ActivityWatch into a successful project.
-    | Early users like you mean a lot to us, and we hope you'll reach out to us with any ideas you have for improvements!
-  p
-    | If you are a developer, we hope you can contribute by writing a watcher, visualization, or something else, and share it with us on the forum!
-  p
-    div Thank you for using ActivityWatch!
-    small If you have a minute to spare, please take the time to fill out our #[a(href="https://forms.gle/q2N9K5RoERBV8kqPA") user survey] or #[a(href="https://forum.activitywatch.net/c/features") vote on features on the forum].
+  div.container.mt-4
+    h1.text-center Welcome to Reclaim
+    h4.text-center AI-Assisted Digital Dependency Detection System
 
-  hr
+    p.text-center
+      | Reclaim monitors digital activity, analyzes screen-time patterns,
+      | detects digital dependency levels, and generates AI-powered monthly reports.
 
-  div.row
-    div.col-md-6
-      h4 Spread the word
-      p
-        | Nothing is as motivating as getting ActivityWatch into the hands of users.
-        | By sharing it you get us to make ActivityWatch even better!
-      ul
-        li Give us a like and a review on #[a(href="https://alternativeto.net/software/activitywatch/") AlternativeTo]
-        li Vote on us at #[a(href="https://www.producthunt.com/posts/activitywatch") ProductHunt]
-        li Follow us and spread the word on #[a(href="https://twitter.com/ActivityWatchIt") Twitter] and #[a(href="https://www.facebook.com/ActivityWatch") Facebook]
-        li Star us on #[a(href="https://github.com/ActivityWatch/activitywatch") GitHub]
+    hr
 
-    div.col-md-6
-      h4 Support us!
-      p
-        | Do you like ActivityWatch? Has it helped you? Help us help you by donating!
-        | You can donate to us via:
-      ul
-        li #[a(href="https://www.patreon.com/erikbjare") Patreon]
-        li #[a(href="https://opencollective.com/activitywatch") Open Collective]
-        li #[a(href="https://liberapay.com/ActivityWatch/") Liberapay]
-        li #[a(href="https://activitywatch.net/donate/") Other methods] (incl. cryptocurrency)
-      p
-        | For more info, please visit the #[a(href="https://activitywatch.net/donate/") donation page on the website].
+    div.row
+      div.col-md-6
+        h4 Activity Monitoring
+        p
+          | Track application usage, active hours, screen time,
+          | and digital behavior patterns in real time.
 
-  hr
+      div.col-md-6
+        h4 AI-Based Analysis
+        p
+          | Analyze activity data to identify excessive usage,
+          | peak activity periods, and dependency indicators.
 
-  div.row
-    div.col-md-6
-      h4 Resources
-      p
+    hr
+
+    div.row
+      div.col-md-6
+        h4 Monthly Behavioral Reports
         ul
-          li #[a(href="https://activitywatch.net/") Website]
-          li #[a(href="https://activitywatch.readthedocs.org/") Documentation]
-          li #[a(href="https://forum.activitywatch.net/") Forum]
-          li #[a(href="https://discord.gg/vDskV9q") Discord]
-          li #[a(href="https://github.com/ActivityWatch/activitywatch") GitHub]
-          li(v-if="!info.version.includes('rust')" ) #[a(href="/api/") API Browser]
+          li Total screen time
+          li Most used applications
+          li Peak usage hours
+          li Daily activity summaries
+          li Monthly digital wellness score
 
-    div.col-md-6
-      h4 Want to know what we're working on?
-      p
-        | Check out the #[a(href="https://forum.activitywatch.net/c/news") development updates]!
+      div.col-md-6
+        h4 Personalized Recommendations
+        ul
+          li Reduce excessive screen time
+          li Improve productivity habits
+          li Optimize digital well-being
+          li Healthy usage suggestions
+          li Parent and guardian insights
 
-  hr
+    hr
 
-  p
-    small
-      i
-        | You can change which page opens when you open ActivityWatch (instead of this page) in the #[router-link(to="/settings") settings].
+    div.row
+      div.col-md-12
+        h4 Key Features
+        ul
+          li Activity and screen-time monitoring
+          li Peak usage detection
+          li Monthly report generation
+          li Digital dependency scoring
+          li AI-powered recommendations
+          li Parent monitoring support
+
+    hr
+
+    p.text-center
+      small
+        i
+          | Configure system preferences from the #[router-link(to="/settings") settings] page.
 
 </template>
 
